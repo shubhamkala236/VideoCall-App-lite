634e32e21812469ab07ac129e2f3be1f4c08c29f
Shubham <lawkktd@gmail.com>
2021-05-28 20:13:24 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express')
+var app = express()
+const server = require("http").createServer(app);
+var cors = require('cors')
+
+app.use(cors())
+
+
+const io = require('socket.io')(server,{
+    cors: {
+        origin:'*',
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.send('Server is running');
+});
+
+io.on('connection',(socket)=>{
+    socket.emit('me',socket.id);
+
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit("callEnded");
+    });
+
+    socket.on('calluser',({ userToCall,signalData,from,name })=>{
+        io.to(userToCall).emit('calluser',{signal:signalData , from,name});
+
+    });
+
+    socket.on('answercall',(data)=>{
+        io.to(data.to).emit('callaccepted',data.signal)
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
