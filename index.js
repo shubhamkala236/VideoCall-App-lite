e3f46cb09ef647a64d4497b561a700971e24280b
shubham <lawkktd@gmail.com>
2021-06-15 22:20:37 +0530

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
-
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
@@ -15,11 +14,8 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-	res.send("YOOO");
+	res.send('Server is running');
 });
-// app.get('/', (req, res) => {
-// 	res.send('Running file');
-// });
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
